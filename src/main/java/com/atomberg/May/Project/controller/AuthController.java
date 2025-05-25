402dbc7359c27edfef106008d2aade81baf6dd14
santhosh3899 <santhoshk8680@gmail.com>
2025-05-25 21:52:24 +0530

    Email service tested

--- src/main/java/com/atomberg/May/Project/controller/AuthController.java
+++ src/main/java/com/atomberg/May/Project/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.method.MethodValidationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.atomberg.May.Project.controller.pojo.LoginApiData;
 import com.atomberg.May.Project.controller.pojo.SignupApiData;
 import com.atomberg.May.Project.controller.service.AuthService;
+import com.atomberg.May.Project.controller.service.EmailService;
 import com.atomberg.May.Project.entity.User;
 
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 
 @RestController
@@ -25,6 +28,9 @@ public class AuthController {
 	
 	@Autowired
 	public AuthService authService;
+	
+	@Autowired
+	public EmailService emailService;
 	
 	@PostMapping("create-account")
 	public Object createAccount(@Valid@RequestBody SignupApiData signupApiData) throws Exception {
@@ -46,6 +52,33 @@ public class AuthController {
 		responseMap.put("User Data", userDetails);
 		return ResponseEntity.status(HttpStatus.OK).body(responseMap);
 		
+	}
+	
+	@GetMapping("/send-email")
+	public ResponseEntity<?> sendEmail() throws MessagingException{
+		Map<String, String> responseMap = new HashMap<String, String>();
+		
+		String fromEmail="santhoshk8680@gmail.com";
+		String toEmail="santhoshk8680@gmail.com";
+		String subject = "Test email";
+		String mailbody = "Hello, This is test email from spring boot";
+		
+		
+		  emailService.sendPlainEmail(fromEmail, toEmail, subject, mailbody);
+		  
+		  subject="Test HTML email"; mailbody = "<!DOCTYPE html>" + "<html>" + "<body>"
+		  + "<p>Hello Santhosh,</p>" +
+		  "<p>This is a test email sent using <strong>Spring Boot</strong>.</p>" +
+		  "<p>Regards,<br/>Your Company</p>" + "</body>" + "</html>";
+		  
+		  emailService.sendHtmlEmail(fromEmail, toEmail, subject, mailbody);
+		 
+		
+		emailService.sendTemplateEmail(fromEmail, toEmail,subject, "test-email");
+		
+		responseMap.put("Result", "Success");
+		responseMap.put("Message", "Email sent");
+		return ResponseEntity.status(HttpStatus.OK).body(responseMap);
 	}
 		
 }
--- src/main/java/com/atomberg/May/Project/controller/service/EmailService.java
+++ src/main/java/com/atomberg/May/Project/controller/service/EmailService.java
@@ -3,7 +3,13 @@ package com.atomberg.May.Project.controller.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -11,13 +17,42 @@ public class EmailService {
 	@Autowired
 	public JavaMailSender javaMailSender;
 	
-	public void sendPlainEmail(String fromEmail, String toEmail, String mailBody, String subject) {
+	@Autowired
+	public TemplateEngine templateEngine;
+	
+	public void sendPlainEmail(String fromEmail, String toEmail, String subject, String mailBody) {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setFrom(fromEmail);
 		message.setTo(toEmail);
 		message.setSubject(subject);
 		message.setText(mailBody);
 		javaMailSender.send(message);
+	}
+	
+	public void sendHtmlEmail(String fromEmail, String toEmail, String subject, String mailBody) throws MessagingException {
+		MimeMessage message = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+		helper.setFrom(fromEmail);
+		helper.setTo(toEmail);
+		helper.setSubject(subject);
+		helper.setText(mailBody,true);
+		javaMailSender.send(message);
+	}
+	
+	public void sendTemplateEmail(String fromEmail, String toEmail, String subject, String fileName) throws MessagingException {
+		
+		Context context = new Context();
+		
+		String mailBody = templateEngine.process(fileName, context);
+		
+		MimeMessage message = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+		helper.setFrom(fromEmail);
+		helper.setTo(toEmail);
+		helper.setSubject(subject);
+		helper.setText(mailBody,true);
+		javaMailSender.send(message);
+		
 	}
 
 }
